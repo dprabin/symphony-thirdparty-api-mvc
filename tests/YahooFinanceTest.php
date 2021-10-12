0d013cf95c29964ff84fecdde19f81c2502099bc
Prabin Babu Dhakal <dprabin@yahoo.com>
2021-10-12 06:52:05 +0545

    Initial test

---
+++ tests/YahooFinanceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class YahooFinanceTest extends KernelTestCase
+{
+    /** @test */
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
