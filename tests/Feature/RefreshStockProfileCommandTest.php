f8763fd0e6fe81953f3e5a9f8c5f58aa1b6da5b3
Prabin Babu Dhakal <dprabin@yahoo.com>
2021-10-12 07:41:04 +0545

    update test function

--- tests/Feature/RefreshStockProfileCommandTest.php
+++ tests/Feature/RefreshStockProfileCommandTest.php
@@ -3,15 +3,63 @@
 namespace App\Tests\Feature;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Console\Tester\CommandTester;
+
+use App\Entity\Stock;
+use App\Tests\DatabasePrimer;
 
 class RefreshStockProfileCommandTest extends KernelTestCase
 {
-    public function testSomething(): void
+    private $entityManager;
+
+    protected function setUp(): void
     {
         $kernel = self::bootKernel();
+        DatabasePrimer::prime($kernel);
 
-        $this->assertSame('test', $kernel->getEnvironment());
-        //$routerService = self::$container->get('router');
-        //$myCustomService = self::$container->get(CustomService::class);
+        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    /** @test */
+    public function test_whether_refresh_stock_profile_command_behaves_correctly(): void
+    {
+        //setup
+        $stock = new Stock;
+        $application = new Application(self::$kernel);
+
+        //command
+        $command = $application->find('app:refresh-stock-profile');
+
+        $commandTester = new CommandTester($command);
+
+        //Do something
+        $commandTester->execute([
+            'symbol' =>'AMZN',
+            'region' => 'US'
+        ]);
+
+        //Make Assertions
+        //DB assertions
+        $repo->$this->entityManager->getRepository(Stock::class);
+
+        /** @var Stock $stock */
+        $stock = $repo->findOneBy(['symbol'=>'AMZN']);
+
+        $this->assertSame('USD',$stock->getCurrency());
+        $this->assertSame('NasdaqGS',$stock->getExchangeName());
+        $this->assertSame('AMZN',$stock->getSymbol());
+        $this->assertSame('Amazon.com, Inc.',$stock->getShortName());
+        $this->assertSame('US',$stock->getRegion());
+        $this->assertGreaterThan(50,$stock->getPrice());
+        $this->assertGreaterThan(50,$stock->getPreviousClose());
+
     }
 }
